f10d8843004e44d530d1fd3a4be3f93769d903bf
Leopold Talirz <leopold.talirz@gmail.com>
2018-04-04 10:35:28 +0200

    update links

--- start.py
+++ start.py
@@ -9,24 +9,25 @@ def get_start_widget(appbase, jupbase, notebase):
         <th style="width:70px" rowspan=2></th>
         <th style="text-align:center">Querying the Database</th>
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Other examples</th>
+        <th style="text-align:center">Advanced features</th>
     <tr>
     <td valign="top"><ul>
+    <li><a href="{jupbase}/notebooks/retrieve_structure_from_external_db.ipynb" target="_blank">Load a structure</a>
     <li><a href="{jupbase}/notebooks/submit_pw_calculation.ipynb" target="_blank">Submit a calculation</a>
-    <li><a href="{jupbase}/notebooks/graph_visualization.ipynb" target="_blank">Graph Visualization</a>
-    <li><a href="{jupbase}/notebooks/export_import.ipynb" target="_blank">Importing and Exporting Data</a>
+    <li><a href="{jupbase}/notebooks/graph_visualization.ipynb" target="_blank">Visualize the provenance graph</a>
     </ul></td>
     
     <td valign="top"><ul>
-    <li><a href="{jupbase}/notebooks/query_database_statistics.ipynb" target="_blank">Statistics</a>
-    <li><a href="{jupbase}/notebooks/query_database_statistics.ipynb" target="_blank">Screening</a>
-    <li><a href="{jupbase}/notebooks/query_database_statistics.ipynb" target="_blank">Structures Example</a>
+    <li><a href="{jupbase}/notebooks/query_database_statistics.ipynb" target="_blank">Nodes and their links</a>
+    <li><a href="{jupbase}/notebooks/query_structures_from_code.ipynb" target="_blank">Codes and Calculations</a>
+    <li><a href="{jupbase}/notebooks/query_screening_database.ipynb" target="_blank">Electronic properties of perovskites</a>
     </ul></td>
 
     <td valign="top"><ul>
-    <li><a href="{jupbase}/notebooks/equation_of_states.ipynb" target="_blank">Equation of state</a>
-    <li><a href="{jupbase}/notebooks/sssp_seekpath_demo.ipynb" target="_blank">SSSP seekpath demo</a>
-    <li><a href="{jupbase}/notebooks/retrieve_structure_from_external_db.ipynb" target="_blank">External databases</a>
+    <li><a href="{jupbase}/notebooks/sssp_seekpath_demo.ipynb" target="_blank">Automatic band structures</a>
+    <li><a href="{jupbase}/notebooks/equation_of_states.ipynb" target="_blank">Equation of state workflow</a>
+    <li><a href="{jupbase}/notebooks/equation_of_states_interactive.ipynb" target="_blank">Interactive equation of state</a>
+    <li><a href="{jupbase}/notebooks/export_import.ipynb" target="_blank">Importing and Exporting Nodes</a>
     </ul></td>
 
     </tr></table>
