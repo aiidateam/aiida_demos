5de123a0e728de291602c16576cfeac266b0f990
Sebastiaan Huber <mail@sphuber.net>
2018-05-25 11:50:20 +0200

    Do not explicitly call .process() before passing to launchers
    The new launchers, `run` and `submit` can deal with a JobCalculation
    class and will automatically convert it to a JobProcess. Therefore it
    is best not to do this manually as it is unnecessary and will confuse
    the users. Updated this in the simple_sync_workflow.py script

--- tutorial/scripts/simple_sync_workflow.py
+++ tutorial/scripts/simple_sync_workflow.py
@@ -5,24 +5,23 @@ from aiida.work import workfunction as wf
 from aiida.orm.data.base import Str, Float
 from aiida.orm import CalculationFactory, DataFactory
 
+# Load the calculation class 'PwCalculation' using its entry point 'quantumespresso.pw'
 PwCalculation = CalculationFactory('quantumespresso.pw')
 
 scale_facs = (0.96, 0.98, 1.0, 1.02, 1.04)
-labels = ["c1", "c2", "c3", "c4", "c5"]
+labels = ['c1', 'c2', 'c3', 'c4', 'c5']
 
 @wf
 def run_eos_wf(codename, pseudo_family, element):
     print "Workfunction node identifiers: {}".format(Process.current().calc)
-    #Instantiate a JobCalc process and create basic structure
-    JobCalc = PwCalculation.process()
     s0 = create_diamond_fcc(Str(element))
     
     calcs = {}
     for label, factor in zip(labels, scale_facs):
-        s = rescale(s0,Float(factor))
+        s = rescale(s0, Float(factor))
         inputs = generate_scf_input_params(s, str(codename), Str(pseudo_family))
         print "Running a scf for {} with scale factor {}".format(element, factor)
-        result = run(JobCalc,**inputs)
+        result = run(PwCalculation, **inputs)
         print "RESULT: {}".format(result)
         calcs[label] = get_info(result)
 
@@ -30,8 +29,8 @@ def run_eos_wf(codename, pseudo_family, element):
     for label in labels:
         eos.append(calcs[label])
     
-    #Return information to plot the EOS
-    ParameterData = DataFactory("parameter")
+    # Return information to plot the EOS
+    ParameterData = DataFactory('parameter')
     retdict = {
             'initial_structure': s0,
             'result': ParameterData(dict={'eos_data': eos})
@@ -45,7 +44,7 @@ def get_info(calc_results):
             calc_results['output_parameters'].dict.energy,
             calc_results['output_parameters'].dict.energy_units)
 
-def run_eos(codename='qe-pw-6.2.1@localhost', pseudo_family='GBRV_lda', element="Si"):
+def run_eos(codename='qe-pw-6.2.1@localhost', pseudo_family='GBRV_lda', element='Si'):
     return run_eos_wf(Str(codename), Str(pseudo_family), Str(element))
 
 if __name__ == '__main__':
